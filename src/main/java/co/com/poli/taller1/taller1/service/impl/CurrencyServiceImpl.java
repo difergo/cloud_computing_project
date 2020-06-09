5cfa3f343b7aadf4c21749a1a76ea88e8e94719d
Diego Fernando Gomez Alvarez <diego.gomez@sophossolutions.com>
2020-06-08 19:11:02 -0500

    Etiqueta @Service en CurrencyServiceImpl

--- src/main/java/co/com/poli/taller1/taller1/service/impl/CurrencyServiceImpl.java
+++ src/main/java/co/com/poli/taller1/taller1/service/impl/CurrencyServiceImpl.java
@@ -4,7 +4,9 @@ package co.com.poli.taller1.taller1.service.impl;
 import co.com.poli.taller1.taller1.domain.Currency;
 import co.com.poli.taller1.taller1.service.CurrencyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CurrencyServiceImpl implements CurrencyService {
 
     @Autowired
