071faf9b30c4c2d6c525588c7e16307d2bc45013
Juan Valencia <juan_valencia82131@elpoli.edu.co>
2020-06-09 23:05:53 -0500

    pruebaException

---
+++ src/main/java/co/com/poli/taller1/taller1/service/MapValidationErrorService.java
@@ -0,0 +1,36 @@
+package co.com.poli.taller1.taller1.service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidationErrorService {
+
+    /**
+     *@return si result tiene errores entonces devuelve un Map con la
+     *estructura "atributo":"error". De lo contrario null
+     */
+    public ResponseEntity<?> MapValidationService(BindingResult result) {
+
+        // en caso de error hago que el json q me devuelve tenga la estructura "field": "errormessage"
+        if (result.hasErrors()) {
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error : result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        return null;
+    }
+
+}
