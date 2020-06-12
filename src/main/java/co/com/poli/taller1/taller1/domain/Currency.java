2b80df17fd3a917471e34572b3a3b1aedb3d85a7
Juan Valencia <juan_valencia82131@elpoli.edu.co>
2020-06-11 19:29:24 -0500

    Correcion Busqueda Currencies

--- src/main/java/co/com/poli/taller1/taller1/domain/Currency.java
+++ src/main/java/co/com/poli/taller1/taller1/domain/Currency.java
@@ -1,6 +1,7 @@
 package co.com.poli.taller1.taller1.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,16 +22,17 @@ public class Currency {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private long id;
-        @NotBlank(message="El Nombre es incorrecto")
         @NotEmpty(message = "El Nombre no debe ser vacio")
+        @NotBlank(message="El Nombre es incorrecto")
         @Column(updatable = false, unique = true)
         private String name;
-        @NotBlank(message="El Nombre es incorrecto")
         @NotEmpty(message = "El Nombre no debe ser vacio")
+        @NotBlank(message="El Nombre es incorrecto")
         @Column(updatable = false, unique = true)
         private String symbol;
         private int rank;
 
         @OneToMany(cascade = CascadeType.MERGE, mappedBy = "currency")
+        @JsonManagedReference
         private List<Quote> quoteList;
 }
--- src/main/java/co/com/poli/taller1/taller1/domain/Quote.java
+++ src/main/java/co/com/poli/taller1/taller1/domain/Quote.java
@@ -1,6 +1,8 @@
 package co.com.poli.taller1.taller1.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,11 +24,11 @@ public class Quote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long Id;
-    @NotBlank(message="El Nombre es incorrecto")
     @NotEmpty(message = "El Nombre no debe ser vacio")
+    @NotBlank(message="El Nombre es incorrecto")
     private String name;
-    @NotBlank(message="El Symbol es incorrecto")
     @NotEmpty(message = "El Symbol no debe ser vacio")
+    @NotBlank(message="El Symbol es incorrecto")
     private String symbol;
     @Positive(message = "El precio debe ser mayor a cero")
     private double price;
@@ -35,5 +37,7 @@ public class Quote {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="currency_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+    @JsonBackReference
     private Currency currency;
+
 }
